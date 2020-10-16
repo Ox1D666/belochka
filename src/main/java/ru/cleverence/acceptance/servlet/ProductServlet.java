d2404f151ddf1dfd3a60c2cabff7f7c13c23bcbc
Kirill Romanov <kirill.coldline@gmail.com>
2020-10-16 11:09:03 +0300

    create project belochka

---
+++ src/main/java/ru/cleverence/acceptance/servlet/ProductServlet.java
@@ -0,0 +1,20 @@
+package ru.cleverence.acceptance.servlet;
+
+import ru.cleverence.acceptance.model.Product;
+import ru.cleverence.acceptance.store.MemStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ProductServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        MemStore.instOf().save(new Product(0, req.getParameter("name"),
+                Integer.parseInt(req.getParameter("quantity")), req.getParameter("desc")));
+        resp.sendRedirect(req.getContextPath() + "/product/edit.do");
+    }
+}
---
+++ src/test/java/ru/cleverence/acceptance/store/MemStoreTest.java
@@ -0,0 +1,12 @@
+package ru.cleverence.acceptance.store;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemStoreTest {
+    @Test
+    public void whenAddThreeProduct() {
+
+    }
+}
